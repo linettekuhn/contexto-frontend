feee0b6cff88a3e49a4b5eadb1d4beef65e2f9c3
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2026-02-20 23:47:14 -0500

    Implement navigation bar, base styles, theme switcher component, and home screen header

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import ThemeToggle from "./icons/Lamp";
+import styles from "./Navbar.module.css";
+import Logo from "./icons/Logo";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className={styles.navBar}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+}

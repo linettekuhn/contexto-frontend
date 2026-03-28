5b6740234b63dc061c4d39dd8286c37f9a84a849
Linette Kuhn <linette.kuhn@gmail.com>
2026-03-28 19:32:06 -0400

    Fix base url in HistoryService

--- src/services/HistoryService.ts
+++ src/services/HistoryService.ts
@@ -1,7 +1,7 @@
 import type { BackendError, HistoryItem } from "../types";
 import { authFetch } from "./AuthService";
 
-const BASE_URL = "http://localhost:3009";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function getHistory(): Promise<HistoryItem[]> {
   // call history endpoint in backend

a1307ada20cb2a49413faf6e2b4009d29c96d8ee
Linette Kuhn <linette.kuhn@gmail.com>
2026-03-28 18:19:11 -0400

    Update config to use proxy fowarding

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // use vite proxy for dev proxy fowarding
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3009",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
